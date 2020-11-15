633170b0f42e93012d904895fac5c22d0e667c60
akima9 <akima9a@gmail.com>
2020-11-15 18:45:04 +0900

    commit

---
+++ node_project/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const hbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const server = express();
+const words = require("./db/words.json");
+
+server.engine(
+    "hbs", 
+    hbs({
+        extname:"hbs",
+        defaultLayout:"layout.hbs",
+        partialsDir:"partials"
+    })
+);
+
+server.set("view engine", "hbs");
+server.use(express.static(__dirname+"/public"));
+server.use(bodyParser.urlencoded({extended:false}));
+
+server.get("/",(req,res)=>{
+    res.render("home",{
+        words
+    });
+});
+
+server.post("/",(req,res)=>{
+    res.render("home",{
+        words:words.filter(w=>w.word.toLowerCase().includes(query.toLowerCase()))
+    })
+});
+
+server.get("/add", (req,res)=>{
+    res.render("add");
+});
+
+server.get("/quiz", (req,res)=>{
+    res.render("quiz");
+});
+
+server.use((req,res)=>{
+    res.render("404");
+});
+
+server.listen(3000,(err)=>{
+    if(err) return console.log(err);
+    console.log("The server is listening on port 3000");
+});
